6ecb9a66cbe901cc194c6dd9812724f9fe57dae4
tipampili <128613317+tipampili@users.noreply.github.com>
2023-06-16 12:05:21 -0300

    Add files via upload

---
+++ instalar.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+chmod -R 755 LCD-show
+sudo cp kiosk.sh /home/pi
+sudo cp kiosk.service /lib/systemd/system/
+sudo systemctl enable kiosk.service
+sudo cp lightdm.conf /etc/lightdm
+sudo apt-get install unclutter
+unclutter -idle 0.01 -root
+reboot
